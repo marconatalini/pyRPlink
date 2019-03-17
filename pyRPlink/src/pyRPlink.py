4ce80962bc3deb7d2750f07af8236cad367254ea
Marco Natalini <marconatalini.75@gmail.com>
2019-03-18 00:04:07 +0100

    Check all dipendenti, move file to log if succesfull

--- pyRPlink/src/pyRPlink.py
+++ pyRPlink/src/pyRPlink.py
@@ -3,8 +3,11 @@ Created on 17 mar 2019
 
 @author: Marco
 '''
+
 from Codici import Codici
 from Registrazione import Registrazione
+import shutil
+from _datetime import datetime
 import os
 
 print("""
@@ -23,20 +26,36 @@ print("    Caricato {} dipendenti.".format(len(dipendenti.getCodici())))
 print("    Ultimo dipendente: {} {}".format(dipendenti.getLastNomeDipendente(),dipendenti.getCodici()[-1].rfidCode))
 
 print("Creo il file da importare...", end=' ')
-timbrature = open("TIMBRATURE.TXT", "w")
+timbPath = "TIMBRATURE.TXT"
+timbrature = open(timbPath, "w")
 print("OK")
 
 print("Carico le timbrature dai terminali...")
 registrazioni = []
-for line in open("TRANSACTIONS.TXT", "r"):
+errori = []
+ftpFilepath = "TRANSACTIONS.TXT"
+logPath = "log"
+ftr = open(ftpFilepath, "r")
+
+for line in ftr:
     timbratura = Registrazione(line)
     dipendente = dipendenti.findDipendenteByRfid(timbratura.getCode())
     if dipendente:
-        registrazioni.append(timbratura.getConvertedString(dipendente.getAScode())) 
+        registrazioni.append(timbratura.getConvertedString(dipendente.getAScode()))
+    else:
+        errori.append(line)
+        
+ftr.close()
 
-print("Converto i codici dipendente e scrivo il file da importare...", end=' ')
-timbrature.write("\n".join(registrazioni))
-print("OK")
+if len(errori):
+    print("Il file dipendenti non era completo. Completalo e riprova.", end=' ')
+else:
+    print("Converto i codici dipendente e scrivo il file da importare...", end=' ')
+    timbrature.write("\n".join(registrazioni))
+    timbrature.close()
+    shutil.move(ftpFilepath, os.path.join(logPath,"{}.txt".format(str(datetime.utcnow()).replace(':',''))))
+    print("OK")
+
 
 
 if __name__ == '__main__':
